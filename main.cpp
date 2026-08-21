e57e75665ec723db4d1b4b81a9a1d4a2a14a20d3
bodmeeen <maks.demianp@gmail.com>
2026-08-21 23:53:14 +0300

    Зроблено поля в матриці

--- main.cpp
+++ main.cpp
@@ -6,21 +6,20 @@ int main() {
 
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
-            matrix[y][x] = '#';
+            if (y == 0 || y == 9 || x == 0 || x == 9) {
+                matrix[y][x] = '#';
+            } else {
+                matrix[y][x] = ' ';
+            }
         }
     }
 
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout << matrix[i][j] << " ";
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
+            cout << matrix[y][x] << " ";
         }
-    cout << endl;
+        cout << endl;
     }
 
-    cout << sizeof(matrix) << " bytes\n";
-    
-    
-    
     return 0;
 }
